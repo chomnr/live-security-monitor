2dadb9b42040552e5038f9ba544013a805cf5229
chomnr <zeljkovranjes@outlook.com>
2023-06-10 20:20:14 -0400

    modified file name & directory.

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,8 +1,8 @@
 public class Constants {
     public static final int WEBSOCKET_PORT = 8080;
 
-    public static final String  METRIC_FILE_LOCATION = "/Users/zeljk/OneDrive/Desktop/sample.json";
+    public static final String  METRIC_FILE_LOCATION = "/Users/zeljk/OneDrive/Desktop/Test/sample.json";
     public static final String FILE_DIRECTORY = "/Users/zeljk/OneDrive/Desktop/Test/";
-    public static final String FILE = "hello.txt";
+    public static final String FILE = "attempts_log.txt";
 
 }
