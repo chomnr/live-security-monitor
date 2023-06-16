0bc35fe1334f38d140b42ae27697ed8768b5c459
chomnr <zeljkovranjes@outlook.com>
2023-06-15 22:25:09 -0400

    moved to different directory.

---
+++ src/main/java/Brute/Constants.java
@@ -0,0 +1,12 @@
+package Brute;
+
+public class Constants {
+    public static final int WEBSOCKET_PORT = 8080;
+
+    public static final String METRIC_FILE_LOCATION = "/Users/zeljk/OneDrive/Desktop/Test/sample.json";
+
+    public static final String LOG_FILE_DIRECTORY = "/Users/zeljk/OneDrive/Desktop/Test/";
+    public static final String LOG_FILE = "attempts_log.txt";
+    public static final String LOG_FILE_LOCATION = LOG_FILE_DIRECTORY + LOG_FILE;
+
+}
--- src/main/java/Constants.java
+++
@@ -1,8 +0,0 @@
-public class Constants {
-    public static final int WEBSOCKET_PORT = 8080;
-
-    public static final String  METRIC_FILE_LOCATION = "/Users/zeljk/OneDrive/Desktop/Test/sample.json";
-    public static final String FILE_DIRECTORY = "/Users/zeljk/OneDrive/Desktop/Test/";
-    public static final String FILE = "attempts_log.txt";
-
-}
