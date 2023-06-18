0a346e00ee4f733d752438e7c5216835e2d00ae5
chomnr <zeljkovranjes@outlook.com>
2023-06-17 20:59:08 -0400

    added country.mmdb

--- src/main/java/Brute/Constants.java
+++ src/main/java/Brute/Constants.java
@@ -5,12 +5,13 @@ import java.time.ZoneOffset;
 public class Constants {
     public static final int WEBSOCKET_PORT = 8080;
 
+    public static final String COUNTRY_FILE_LOCATION = "country.mmdb";
     public static final ZoneOffset ZONE_OFFSET = ZoneOffset.UTC;
 
-    public static final String METRIC_FILE_LOCATION = "/Users/zeljk/OneDrive/Desktop/Test/sample.json";
+    public static final String METRIC_FILE_LOCATION = "/Users/zeljk/OneDrive/Desktop/Test/metrics.json";
 
     public static final String LOG_FILE_DIRECTORY = "/Users/zeljk/OneDrive/Desktop/Test/";
-    public static final String LOG_FILE = "attempts_log.txt";
+    public static final String LOG_FILE = "brute_log.txt";
     public static final String LOG_FILE_LOCATION = LOG_FILE_DIRECTORY + LOG_FILE;
 
 
