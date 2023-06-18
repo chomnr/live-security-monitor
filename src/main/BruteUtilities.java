2667c271c89c43b649f470f3ea060fed0156a934
chomnr <zeljkovranjes@outlook.com>
2023-06-17 20:59:40 -0400

    simplified print function.

--- src/main/BruteUtilities.java
+++ src/main/BruteUtilities.java
@@ -4,10 +4,8 @@ public class BruteUtilities {
 
     public static String PREFIX = "BruteExpose: ";
     public static void print(String message) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(PREFIX);
-        sb.append("");
-        sb.append(message);
+        String sb = PREFIX +
+                message;
         System.out.println(sb);
     }
 }
