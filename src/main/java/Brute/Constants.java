a0aa8df401fa348b43fd2b93955d8c406f780f2b
chomnr <zeljkovranjes@outlook.com>
2023-06-29 21:16:22 -0400

    temporarily.

--- src/main/java/Brute/Constants.java
+++ src/main/java/Brute/Constants.java
@@ -8,15 +8,18 @@ public class Constants {
     public static final String COUNTRY_FILE_LOCATION = "country.mmdb";
     public static final ZoneOffset ZONE_OFFSET = ZoneOffset.UTC;
 
+    // Metrics File
     //public static final String METRIC_FILE_LOCATION = "/Users/zeljk/OneDrive/Desktop/Test/brute_metrics.json";
-    public static final String METRIC_FILE_LOCATION = "/var/log/brute_metrics.json";
+    public static final String METRIC_FILE_DIRECTORY = "/var/log/";
+    public static final String METRIC_FILE = "brute_metrics.json";
+    public static final String METRIC_FILE_LOCATION = METRIC_FILE_DIRECTORY + METRIC_FILE;
 
-    //public static final String TRACKER_FILE_DIRECTORY = "/Users/zeljk/OneDrive/Desktop/Test/"; // MAKE SURE THE DIRECTORY FOR BE_LOG_FILE ARE THE SAME. windows variant
-    public static final String TRACKER_FILE_DIRECTORY = "/var/log/";
+    // Tracker File
+    public static final String TRACKER_FILE_DIRECTORY = "/var/log/"; // MAKE SURE THE DIRECTORY FOR BE_LOG_FILE ARE THE SAME. windows variant
     public static final String TRACKER_FILE = "brute_tracker.txt"; // MAKE SURE THE FILE FOR BE_LOG_FILE ARE THE SAME.
     public static final String TRACKER_FILE_LOCATION = TRACKER_FILE_DIRECTORY + TRACKER_FILE;
 
-    //public static final String LOG_FILE_DIRECTORY = "/Users/zeljk/OneDrive/Desktop/Test/";
+    // Log File
     public static final String LOG_FILE_DIRECTORY = "/var/log/";
     public static final String LOG_FILE = "brute_log.json";
     public static final String LOG_FILE_LOCATION = LOG_FILE_DIRECTORY + LOG_FILE;
