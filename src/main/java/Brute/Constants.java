02901d07898cdcdda6650a693d5df3cae19d58e6
chomnr <zeljkovranjes@outlook.com>
2023-06-30 18:01:54 -0400

    removed commented code.

--- src/main/java/Brute/Constants.java
+++ src/main/java/Brute/Constants.java
@@ -48,23 +48,4 @@ public class Constants {
 
         return false;
     }
-
-    // Metrics File
-    //public static final String METRIC_FILE_LOCATION = "/Users/zeljk/OneDrive/Desktop/Test/brute_metrics.json";
-    /*
-    public static String METRIC_FILE_DIRECTORY = BASE_PATH;
-    public static String METRIC_FILE = "brute_metrics.json";
-    public static String METRIC_FILE_LOCATION = METRIC_FILE_DIRECTORY + METRIC_FILE;
-
-    // Tracker File
-    public static String TRACKER_FILE_DIRECTORY = BASE_PATH; // MAKE SURE THE DIRECTORY FOR BE_LOG_FILE ARE THE SAME. windows variant
-    public static String TRACKER_FILE = "brute_tracker.txt"; // MAKE SURE THE FILE FOR BE_LOG_FILE ARE THE SAME.
-    public static String TRACKER_FILE_LOCATION = TRACKER_FILE_DIRECTORY + TRACKER_FILE;
-
-    // Log File
-    public static String LOG_FILE_DIRECTORY = BASE_PATH;
-    public static String LOG_FILE = "brute_log.json";
-    public static String LOG_FILE_LOCATION = LOG_FILE_DIRECTORY + LOG_FILE;
-    public static final int LOG_RETRIEVAL_LIMIT = 50;
-     */
 }
